6d5e7a50cc63dcd8a4903361b3e84620aa960739
John Lopez <jhonfersteven@gmail.com>
2022-10-19 10:26:16 -0500

    instalacion Voyager

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/phpinfo', function() {
+    return phpinfo();
+});
+
+Route::group(['prefix' => 'admin'], function () {
+    Voyager::routes();
+});
