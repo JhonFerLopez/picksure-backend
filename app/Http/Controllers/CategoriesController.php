fa9b0ba2a49d1c6a1eb761cfd1522f5ce371ea9b
JohnFerLopez <jhonfersteven@gmail.com>
2023-03-22 20:51:03 -0500

    Test de Api y Swagger

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -2,9 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\User;
-use App\Models\UserLikeCategory;
-use App\Models\UserLikeImageproduct;
+use App\Models\Language;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\DB;
@@ -12,9 +10,49 @@ use Illuminate\Support\Facades\DB;
 class CategoriesController extends Controller
 {
 
+  /**
+     * @OA\Get(
+     *  tags={"Categorias"},
+     *  summary="Devuelve todos los Categorias filtrando el lenguage recibido",
+     *  description="Retorna un Json con los Datos de las Categorias.",
+     *  path="/api/v1/categories",
+     *  security={{ "bearerAuth": {} }},
+     *  @OA\Parameter(
+     *    name="lenguage",
+     *    in="query",
+     *    description="ID del Lenguage",
+     *    required=true,
+     *    @OA\Schema(
+     *      default="1",
+     *      type="integer",
+     *    )
+     *  ),
+     *  @OA\Response(
+     *    response=200,
+     *    description="Resultado de la Operación",
+     *    @OA\JsonContent(
+     *       @OA\Property(property="status", type="integer", example="200"),
+     *       @OA\Property(type="array",@OA\Items(type="array",@OA\Items())),
+     *    )
+     *  ),
+     *  @OA\Response(
+     *    response=422,
+     *    description="Estado Invalido de la Operación",
+     *    @OA\JsonContent(
+     *       @OA\Property(property="message", type="string", example="Los datos son incorrectos."),
+     *       @OA\Property(property="errors", type="string", example="..."),
+     *    )
+     *  )
+     * )
+     */
 	public function index(Request $request)
   {    	
-    $categories = DB::table('categories')
+    if(!isset($request->lenguage)){
+      return response()->json(array('data' => 'No existe el parametro id Lenguage'), 502);
+    }
+    $idLenguage = Language::where('id', $request->lenguage)->first();
+    if($idLenguage){
+      $categories = DB::table('categories')
       ->join('texts_categories', 'texts_categories.category_id', '=', 'categories.id')
       ->select(
         'categories.id', 
@@ -23,8 +61,54 @@ class CategoriesController extends Controller
       ->where('texts_categories.language_id', '=', $request->lenguage)
       ->orderBy('categories.name', 'asc')
       ->get();
+      $response['status'] = 200;
+      $response['data'] = $categories; 
+    }else {
+      $response['status'] = 402;
+      $response['data'] = 'Lenguaje no existe.';
+    }
+  
+    return response()->json($response, $response['status']);
+  }
 
-    return response()->json($categories, 200);
+  /**
+     * @OA\Get(
+     *  tags={"Categorias"},
+     *  summary="Devuelve todos los Municipios asignados a un departamento",
+     *  description="Retorna un Json con los Datos de los Municipios.",
+     *  path="/api/v1/categories/{prefijo}",
+     *  security={{ "bearerAuth": {} }},
+     *  @OA\Parameter(
+     *    name="prefijo",
+     *    in="path",
+     *    description="prefijo",
+     *    required=true,
+     *    @OA\Schema(
+     *      default="ES",
+     *      type="string",
+     *    )
+     *  ),
+     *  @OA\Response(
+     *    response=200,
+     *    description="Resultado de la Operación",
+     *    @OA\JsonContent(
+     *       @OA\Property(property="id", type="integer", example="1"),
+     *       @OA\Property(property="name", type="string", example="Cali"),
+     *    )
+     *  ),
+     *  @OA\Response(
+     *    response=422,
+     *    description="Estado Invalido de la Operación",
+     *    @OA\JsonContent(
+     *       @OA\Property(property="message", type="string", example="Los datos son incorrectos."),
+     *       @OA\Property(property="errors", type="string", example="..."),
+     *    )
+     *  )
+     * )
+     */
+  public function idioma(Request $request, $prefijo){
+  
+    return response()->json($prefijo, 200);
   }
   
 }
--- routes/api.php
+++ routes/api.php
@@ -8,7 +8,7 @@ use App\Http\Controllers\ImageproductsController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\DepartmentController;
 use App\Http\Controllers\TownController;
-use App\Http\Controllers\CategoriesController;
+use App\Http\Controllers\CategoriesController as Category;
 use App\Http\Controllers\QualifyController;
 
 
@@ -28,21 +28,34 @@ use App\Http\Controllers\QualifyController;
 });
 middleware('auth:sanctum')-> */
 
-
+//Auth
 Route::post('/auth/token', [AuthController::class, 'login']);
+
+//Users
 Route::get('/user/like_category', [UserController::class, 'showLikeCategory']);
 Route::post('/user/like_category', [UserController::class, 'createLikeCategory']);
 Route::delete('/user/like_category', [UserController::class, 'deleteLikeCategory']);
 Route::post('/user/like_imageproduct', [UserController::class, 'createLikeImageproduct']);
 Route::get('/user/like_imageproduct', [UserController::class, 'showLikeImageproduct']);
 Route::delete('/user/like_imageproduct', [UserController::class, 'deleteLikeImageproduct']);
-Route::get('/categories', [CategoriesController::class, 'index']);
+
+//Categories
+Route::get('/categories', [Category::class, 'index']);
+Route::get('/categories/{prefijo}', [Category::class, 'idioma']);
+
+//Departments
 Route::middleware('auth:sanctum')->get('/department', [DepartmentController::class, 'index']);
 //Route::get('/department', [DepartmentController::class, 'index']);
+
+//Towns
 Route::get('/town/department/{id}', [TownController::class, 'department']);
+
+//Qualifies
 Route::get('/qualify', [QualifyController::class, 'index']);
 Route::get('/qualify/{id}', [QualifyController::class, 'rateApp']);
 Route::post('/qualify/{id}', [QualifyController::class, 'createQualifyApp']);
+
+//Images
 Route::get('/imageproducts', [ImageproductsController::class, 'index']);
 Route::get('/imageproducts/get/{id}/', [ImageproductsController::class, 'showOne']);
 Route::get('/imageproducts/category', [ImageproductsController::class, 'categoryId']);
